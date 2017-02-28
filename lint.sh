3298462212e875affafdd8f04b7360cb268c08c8
James Dent <semajdent@gmail.com>
2017-02-28 22:30:24 +1300

    Fixed CMake target directory in lint.sh.

--- lint.sh
+++ lint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 orig_path="$(pwd)"
+script_path="$(dirname $(readlink -f $0))"
 working_dir="build/lint"
 
 mkdir -p "$working_dir"
 cd "$working_dir"
 
-cmake "$(dirname $0)" \
+
+cmake "$script_path" \
 -DCMAKE_C_COMPILER=/usr/bin/clang \
 -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
 -DMYRIAD_ENABLE_LINT=ON
