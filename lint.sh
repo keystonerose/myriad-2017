32eca11f39cf0e4b76f3dc0f10af0fbf7e986091
James Dent <semajdent@gmail.com>
2017-03-11 12:24:59 +1300

    Enabled C++17 support and Clang 5 tooling.

--- lint.sh
+++ lint.sh
@@ -7,12 +7,7 @@ working_dir="build/lint"
 mkdir -p "$working_dir"
 cd "$working_dir"
 
-
-cmake "$script_path" \
--DCMAKE_C_COMPILER=/usr/bin/clang \
--DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
--DMYRIAD_ENABLE_LINT=ON
-
+cmake "$script_path" -DMYRIAD_ENABLE_LINT=ON
 make
 
 cd "$orig_path"
