bd0fdbf0d18a45a33cfa82427fdf6c6babe0e694
James Dent <semajdent@gmail.com>
2017-03-16 21:39:02 +1300

    ~ save

--- src/ksr/error.hpp
+++ src/ksr/error.hpp
@@ -1,25 +1,51 @@
 #ifndef KSR_ERROR_HPP
 #define KSR_ERROR_HPP
 
+#include <sstream>
 #include <stdexcept>
 
 #define KSR_STRINGIFY_DETAIL(x) #x
 #define KSR_STRINGIFY(x) KSR_STRINGIFY_DETAIL(x)
 
 #ifdef NDEBUG
-#define KSR_ASSERT(cond)
+#define KSR_ASSERT(cond) (void)sizeof(cond)
 #else
-#define KSR_ASSERT(cond) \
-    if (!(cond)) throw ksr::logic_error{__FILE__ ":" KSR_STRINGIFY(__LINE__) ": assertion '" #cond "' failed"};
+#define KSR_ASSERT(cond) ksr::detail::debug_assert((cond), #cond, __function__, __FILE__, __LINE__)
 #endif
 
 namespace ksr {
+
     struct logic_error : std::logic_error {
         using std::logic_error::logic_error;
     };
-}
 
-#undef KSR_STRINGIFY
-#undef KSR_STRINGIFY_DETAIL
+    namespace detail {
+
+        ///
+        /// Implementation of the KSR_ASSERT macro when compiling in debug mode. Raises an error
+        /// if \p condition is false.
+        ///
+
+        inline void debug_assert(
+            const bool cond, const char* const cond_str,
+            const char* const function, const char* const file, const int line) {
+
+            if (cond) {
+                return;
+            }
+
+            std::stringstream ss;
+
+
+
+            if (!condition) {
+
+                  __FUNCTION__ "@" __FILE__ ":" KSR_STRINGIFY(__LINE__) ": assertion '" #cond "' failed"
+
+                throw ksr::logic_error{message};
+            }
+        }
+    }
+}
 
 #endif
