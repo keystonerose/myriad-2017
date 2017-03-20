399107c587ff5a9c835d4a1de12d4a908c4454fc
James Dent <semajdent@gmail.com>
2017-03-20 23:02:47 +1300

    ~ save

--- src/ksr/type_traits.hpp
+++ src/ksr/type_traits.hpp
@@ -10,15 +10,67 @@ namespace ksr {
     namespace type_tags {
 
         ///
-        /// Wraps a single type \p T in an object that can be passed to or returned from
+        /// Wraps a single type \p t in an object that can be passed to or returned from
         /// \c constexpr functions performing compile-time operations on types. The tagged type can
-        /// be retrieved from a \ref tag object using \c decltype and the (undefined) member
-        /// function get().
+        /// be retrieved from a \ref tag object using \c decltype.
         ///
 
-        template <typename T>
+        template <typename t>
         struct tag {
-            using type = T;
+            using type = t;
+        };
+
+        template <typename lhs_t, typename rhs_t>
+        constexpr auto operator==(tag<lhs_t>, tag<rhs_t>) -> bool {
+            return std::is_same_v<lhs_t, rhs_t>;
+        }
+
+        template <typename lhs_t, typename rhs_t>
+        constexpr auto operator!=(const tag<lhs_t> lhs, const tag<rhs_t> rhs) -> bool {
+            return !(lhs == rhs);
+        }
+
+        // TODO:DOC
+
+        template <typename...>
+        struct type_seq {
+
+            template <typename t>
+            constexpr auto contains(tag<t>) -> bool { return false; }
+
+            template <typename lhs_t, typename rhs_t>
+            constexpr auto less(tag<lhs_t>, tag<rhs_t>) -> bool { return false; }
+        };
+
+        template <typename head_t, typename... tail_ts>
+        struct type_seq<head_t, tail_ts...> {
+
+            constexpr auto head() -> tag<head_t> { return {}; }
+            constexpr auto tail() -> type_seq<tail_ts...> { return {}; }
+
+            // TODO:DOC
+
+            template <typename t>
+            constexpr auto contains(const tag<t> needle) -> bool {
+
+                if constexpr (head() == needle) {
+                    return true;
+                } else {
+                    return contains(tail(), needle);
+                }
+            }
+
+            // TODO:DOC
+
+            template <typename lhs_t, typename rhs_t>
+            constexpr auto less(const tag<lhs_t> lhs, const tag<rhs_t> rhs) -> bool {
+
+                if constexpr (head() == lhs) {
+                    return seq_contains(tail(), rhs);
+                } else {
+                    return seq_less(tail(), lhs, rhs);
+                }
+            }
         };
     }
 
@@ -36,46 +88,61 @@ namespace ksr {
         /// tags but otherwise has identical behaviour.
         ///
 
-        template <typename T>
-        constexpr auto underlying_type_ext(tag<T>) {
-            if constexpr (std::is_enum_v<T>) {
-                return tag<std::underlying_type_t<T>>{};
+        template <typename t>
+        constexpr auto underlying_type_ext(tag<t>) {
+            if constexpr (std::is_enum_v<t>) {
+                return tag<std::underlying_type_t<t>>{};
             } else {
-                return tag<std::remove_cv_t<T>>{};
+                return tag<std::remove_cv_t<t>>{};
             }
         }
     }
 
-    template <typename T>
-    using underlying_type_ext = decltype(type_tags::underlying_type_ext(type_tags::tag<T>{}));
+    template <typename t>
+    using underlying_type_ext = decltype(type_tags::underlying_type_ext(type_tags::tag<t>{}));
 
-    template <typename T>
-    using underlying_type_ext_t = typename underlying_type_ext<T>::type;
+    template <typename t>
+    using underlying_type_ext_t = typename underlying_type_ext<t>::type;
 
     namespace type_tags {
 
         // TODO:DOC
         // TODO:HERE
 
-        template <typename OutputType, typename InputType>
-        constexpr auto can_narrow(tag<OutputType>, tag<InputType>) -> bool {
+        template <typename output_t, typename input_t>
+        constexpr auto can_narrow(tag<output_t>, tag<input_t>) -> bool {
 
-            using underlying_output_t = underlying_type_ext_t<OutputType>;
-            using underlying_input_t = underlying_type_ext_t<InputType>;
+            if constexpr (std::is_floating_point_v<output_t> && std::is_floating_point_v<input_t>) {
 
-            if constexpr (std::is_integral_v) {
+                constexpr auto floating_point_ranks = type_seq<float, double, long double>{};
+                return floating_point_ranks.less(tag<output_t>{}, tag<input_t>{});
 
             } else {
+
+                using underlying_output_t = underlying_type_ext_t<output_t>;
+                using underlying_input_t = underlying_type_ext_t<input_t>;
+
+                if constexpr (std::is_integral_v<underlying_output_t> && std::is_integral_v<underlying_input_t>) {
+
+                    constexpr auto is_output_signed = std::is_signed_v<underlying_output_t>;
+                    constexpr auto is_input_signed = std::is_signed_v<underlying_input_t>;
+                    return (is_output_signed != is_input_signed) ||
+
+                    // TODO:HERE
+
+                } else {
+                    return false;
+                }
             }
         }
     }
 
-    template <typename OutputType, typename InputType>
+    template <typename output_t, typename input_t>
     struct can_narrow : integral_constant<
-        type_tags::can_narrow(type_tags::tag<OutputType>{}, type_tags::tag<InputType>{})> {};
+        type_tags::can_narrow(type_tags::tag<output_t>{}, type_tags::tag<input_t>{})> {};
 
-    template <typename OutputType, typename InputType>
-    inline constexpr auto can_narrow_v = can_narrow<OutputType, InputType>::value;
+    template <typename output_t, typename input_t>
+    inline constexpr auto can_narrow_v = can_narrow<output_t, input_t>::value;
 }
 
 #endif
--- src/ksr/type_util.hpp
+++ src/ksr/type_util.hpp
@@ -11,31 +11,28 @@ namespace ksr {
     ///
     ///
     /// Uses \c static_cast to perform a narrowing conversion of the value \p input to a
-    /// corresponding value of type \p OutputType. \p InputType and \p OutputType must be types
-    /// presenting a potentially-narrowing conversion as per \ref ksr::can_narrow. For release
-    /// builds, the behaviour of this function is identical to that of \c static_cast and it
-    /// generates no machine code; in debug builds, it raises an error via \c KSR_ASSERT when
-    /// \p input is not representable by \p OutputType.
+    /// corresponding value of type \p output_t. \p input_t and \p output_t must be types presenting
+    /// a potentially-narrowing conversion as per \ref ksr::can_narrow. For release builds, the
+    /// behaviour of this function is identical to that of \c static_cast and it generates no
+    /// machine code; in debug builds, it raises an error via \c KSR_ASSERT when \p input is not
+    /// representable by \p output_t.
     ///
     /// This is an adaptation of the functionality provided by <tt>gsl::narrow()</tt>, but extends
     /// that functionality to also cover enumeration types and, in providing identical behaviour to
     /// \c static_cast for release builds, facilitates more extensive use.
     ///
 
-    template <typename OutputType, typename InputType>
-    constexpr std::enable_if_t<detail::can_narrow_v<InputType, OutputType>, OutputType>
-    narrow_cast(const InputType input) {
+    template <typename output_t, typename input_t>
+    constexpr std::enable_if_t<detail::can_narrow_v<input_t, output_t>, output_t>
+    narrow_cast(const input_t input) {
 
-        static_assert(std::is_arithmetic_v<From> || std::is_enum_v<From>);
-        static_assert(std::is_arithmetic_v<To> || std::is_enum_v<To>);
+        const auto output = static_cast<output_t>(input);
+        KSR_ASSERT(static_cast<input_t>(output) != input);
 
-        const auto output = static_cast<OutputType>(input);
-        KSR_ASSERT(static_cast<InputType>(output) != input);
-
-        constexpr auto is_from_signed = std::is_signed_v<underlying_type_ext_t<From>>;
-        constexpr auto is_to_signed = std::is_signed_v<underlying_type_ext_t<To>>;
+        constexpr auto is_from_signed = std::is_signed_v<underlying_type_ext_t<input_t>>;
+        constexpr auto is_to_signed = std::is_signed_v<underlying_type_ext_t<output_t>>;
         if constexpr (is_from_signed != is_to_signed) {
-            KSR_ASSERT((from < From{}) != (result < To{}));
+            KSR_ASSERT((from < input_t{}) != (result < output_t{}));
         }
 
         return result;
